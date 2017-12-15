1b0c6a0c020be49f43a7a15161725f014c2199b1
Stonga <gaston.robert.auditeur@lecnam.net>
2017-12-15 18:19:13 +0100

    MENU DE GASTON

---
+++ Assets/_Scripts/Menu(Gaston)/Option_Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Option_Menu : MonoBehaviour {
+
+    public GameObject selectButton;
+    public GameObject Back;
+    GameObject myEventSystem;
+
+    public void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject != selectButton && EventSystem.current.currentSelectedGameObject != Back)
+        {
+            EventSystem.current.SetSelectedGameObject(selectButton);
+        }
+    }
+
+    public void Awake()
+    {
+        myEventSystem = GameObject.Find("EventSystem");
+    }
+
+    public void OnEnable()
+    {
+        myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(selectButton);
+    }
+}
